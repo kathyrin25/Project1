d2e629300a3e2e428f658dada7d0272f9cef5abb
kathy.peng <kathyrin25@gmail.com>
2017-08-07 17:45:15 +0800

    finish

--- MyBookkeeping/MyBookkeeping/Controllers/HomeController.cs
+++ MyBookkeeping/MyBookkeeping/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyBookkeeping.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace MyBookkeeping.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -15,16 +17,30 @@ namespace MyBookkeeping.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Page - About.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Page - Contact.";
 
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult ShowHistory()
+        {
+            var history = new List<BookkeepingListViewModel>
+            {
+                new BookkeepingListViewModel {Type=BookType.支出, Amount=300, Date = new DateTime(2017,1,1), Remark="買書" },
+                new BookkeepingListViewModel {Type=BookType.支出, Amount=1600, Date = new DateTime(2017,2,1), Remark="聚餐" },
+                new BookkeepingListViewModel {Type=BookType.支出, Amount=800, Date = new DateTime(2017,2,15), Remark="午餐" },
+            };
+
+            return View(history);
+        }
+        
     }
 }
---
+++ MyBookkeeping/MyBookkeeping/Models/ViewModels/BookkeepingListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyBookkeeping.Models.ViewModels
+{
+    public class BookkeepingListViewModel
+    {
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [Display(Name = "金額")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Display(Name = "日期")]
+        public DateTime Date { get; set; }
+        public string Remark { get; set; }
+
+        [Display(Name = "類別")]
+        public BookType Type { get; set; }
+        
+    }
+
+    public enum BookType
+    {
+        收入,
+        支出
+    }
+}
