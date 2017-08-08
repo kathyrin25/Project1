041a67f92a7cc16f830101e9d030b9e1c6d3e3b1
kathy.peng <kathyrin25@gmail.com>
2017-08-08 16:22:31 +0800

    20170808 commit

--- MyBookkeeping/MyBookkeeping/Controllers/HomeController.cs
+++ MyBookkeeping/MyBookkeeping/Controllers/HomeController.cs
@@ -11,13 +11,12 @@ namespace MyBookkeeping.Controllers
     {
         
         public ActionResult Index()
-        {
-            var Types = Enum.GetValues(typeof(BookType));
-            SelectList category = new SelectList(Types);
+        {          
+            SelectList category = new SelectList(Enum.GetValues(typeof(BookType)));
             List<SelectListItem> _list = category.ToList();
-            _list.Insert(0, new SelectListItem() { Value = "", Text = "請選擇" });      
+            _list.Insert(0, new SelectListItem() { Value = "", Text = "請選擇" }); 
 
-            ViewBag.categoryType = new SelectList((IEnumerable<SelectListItem>)_list, "Value", "Text");
+            ViewData["category"] = new SelectList((IEnumerable<SelectListItem>)_list, "Value", "Text");
             return View();
         }
 
