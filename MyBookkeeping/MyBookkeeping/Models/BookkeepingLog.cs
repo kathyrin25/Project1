bff0e75c11b81c7c814a57c53562db17c8754673
kathy.peng <kathyrin25@gmail.com>
2017-08-17 15:02:49 +0800

    Add new Model

---
+++ MyBookkeeping/MyBookkeeping/Models/BookkeepingLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MyBookkeeping.Models
+{
+    public class BookkeepingLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int BookId { get; set; }
+
+        [StringLength(50)]
+        public string Action { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime UpdateDT { get; set; }
+    }
+}
